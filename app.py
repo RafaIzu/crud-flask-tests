0ba8d4bd2ae532728f73d8808d223c0dbfc2d7d4
RafaIzu <bomizukawa@gmail.com>
2021-08-23 21:47:02 -0300

    fazendo o crud de produtos

--- app.py
+++ app.py
@@ -18,48 +18,123 @@ class Consumer(db.Model):
     id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(150))
     email = db.Column(db.String(150))
+    password = db.Column(db.String(150))
     
-    def __init__(self, name, email):
+    def __init__(self, name, email, password):
         self.name = name
         self.email = email
+        self.password = password
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(150))
+    price = db.Column(db.Float)
+    description = db.Column(db.String(500))
+    players = db.Column(db.Integer)
+    age = db.Column(db.Integer)
+
+    def __init__(self, name, price, description, players, age):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.players = players
+        self.age = age
+
 
 @app.route('/')
 def index():
-    consumer = Consumer.query.all()
+    return render_template('index.html')
+
+
+@app.route('/consumer')
+def consumer():
+    # consumer = Consumer.query.all()
+    consumers = Consumer.query.with_entities(Consumer.id, Consumer.name,
+                                             Consumer.email)
     print(consumer) 
-    return render_template('index.html', consumers=consumer)
+    return render_template('consumer/consumer.html', consumers=consumers)
 
 
-@app.route("/add", methods=["GET", "POST"])
-def add():
+@app.route("/add_consumer", methods=["GET", "POST"])
+def add_consumer():
     if request.method == 'POST':
-        print(request.form['name'])
-        print(request.form['email'])
-        consumer = Consumer(request.form['name'], request.form['email'])
+        consumer = Consumer(request.form['name'], request.form['email'],
+                            request.form['password'])
         db.session.add(consumer)
         db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('add.html')
+        return redirect(url_for('consumer'))
+    return render_template('consumer/add_consumer.html')
 
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
+@app.route('/edit_consumer/<int:id>', methods=['GET', 'POST'])
+def edit_consumer(id):
     consumer = Consumer.query.get(id)
     if request.method == 'POST':
         consumer.name = request.form['name']
         consumer.email = request.form['email']
-        print(">>",request.form['name'])
-        print(">>",request.form['email'])
         db.session.commit()
         print('comitou')
-        return redirect(url_for('index'))
-    return render_template('edit.html', consumer=consumer)
+        return redirect(url_for('consumer'))
+    return render_template('consumer/edit_consumer.html', consumer=consumer)
 
-@app.route('/delete/<int:id>')
-def delete(id):
+@app.route('/delete_consumer/<int:id>')
+def delete_consumer(id):
     consumer = Consumer.query.get(id)
     db.session.delete(consumer)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for('consumer'))
+
+
+@app.route('/product')
+def product():
+    products = Product.query.all()
+    print(product) 
+    return render_template('product/product.html', products=products)
+
+
+@app.route("/add_product", methods=["GET", "POST"])
+def add_product():
+    if request.method == 'POST':
+        product = Product(request.form['name'], request.form['price'],
+                            request.form['description'],
+                            request.form['players'], request.form['age'])
+        db.session.add(product)
+        db.session.commit()
+        return redirect(url_for('product'))
+    return render_template('product/add_product.html')
+
+
+@app.route('/edit_product/<int:id>', methods=['GET', 'POST'])
+def edit_product(id):
+    product = Product.query.get(id)
+    if request.method == 'POST':
+        product.name = request.form['name']
+        product.price = request.form['price']
+        product.description = request.form['description']
+        product.players = request.form['players']
+        product.age = request.form['age']
+        db.session.commit()
+        print('comitou')
+        return redirect(url_for('product'))
+    return render_template('product/edit_product.html', product=product)
+
+
+@app.route('/delete_product/<int:id>')
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    return redirect(url_for('product'))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == "__main__":
     db.create_all()
